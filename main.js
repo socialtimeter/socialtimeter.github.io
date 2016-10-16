dcc2f039d3403995d9de5eb55ba482e5090b5791
jneubrand <johannes_n@icloud.com>
2016-10-15 20:16:09 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,71 @@
+if (window.WebSocket) {
+    var client = new WebSocket('wss://socialtimeter-pabjn.rhcloud.com:8443/')
+
+    function send(client, text) {
+        client.send(text)
+        console.log('out: ' + text)
+    }
+    client.addEventListener('connectFailed', function(e) {
+        console.error('failed' + e.toString())
+    })
+    client.addEventListener('error', function(err) {
+        console.error(err)
+    })
+    client.addEventListener('close', function() {
+        console.log('closed')
+    })
+    client.addEventListener('open', function(message) {
+        console.log('in:  ' + JSON.stringify(message))
+        send(client, '1234')
+    })
+    client.addEventListener('message', function(message) {
+        var data = JSON.parse(message.data)
+        console.log(data)
+        var el = document.getElementById(data.id)
+        if (!el) {
+            el = document.createElement('user')
+            el.id = data.id
+            el.classList.add(data.self ? 'self' : 'other')
+            el.appendChild(document.createElement('arrow'))
+            el.appendChild(document.createElement('name'))
+            el.appendChild(document.createElement('altitude'))
+            document.querySelector('article').appendChild(el)
+        }
+        el.style.backgroundColor = data.color
+        el.querySelector('arrow').style.borderRightColor = data.color
+        el.querySelector('altitude').innerText = data.altitude
+        el.setAttribute('altitude', data.altitude)
+        el.querySelector('name').innerText = data.name
+        realign()
+    })
+} else {
+    document.body.classList.add('nosocket')
+}
+
+function realign() {
+    var list = document.body.querySelector('article')
+    var elements = document.body.querySelectorAll('user')
+    var elArr = []
+    for (var i = 0; i < elements.length; i++) {
+        console.log(i, elements[i], elements[i].nodeType)
+        console.log(elArr)
+        if (elements[i].nodeType == 1) {
+            elArr.push(elements[i])
+        } else {
+            console.log(elements[i])
+            list.removeChild(elements[i])
+        }
+    }
+    elArr.sort(function(a, b) {
+        return b.getAttribute('altitude') - a.getAttribute('altitude')
+    })
+    for (var i = 0; i < elArr.length; i++) {
+        list.appendChild(elArr[i]);
+        if (i < elArr.length - 1) {
+            var spacer = document.createElement('spacer')
+            spacer.style.flexBasis = elArr[i].getAttribute('altitude') -
+                         elArr[i + 1].getAttribute('altitude') + 'px'
+            list.appendChild(spacer)
+        }
+    }
+}
